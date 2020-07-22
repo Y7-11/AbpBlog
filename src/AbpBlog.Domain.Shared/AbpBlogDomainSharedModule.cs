b765dd45e9d70bb0ca2f185125683b6b56a556ec
俊泽 叶 <838079577@qq.com>
2020-07-22 22:31:17 +0800

    修正引用

--- src/AbpBlog.Domain.Shared/AbpBlogDomainSharedModule.cs
+++ src/AbpBlog.Domain.Shared/AbpBlogDomainSharedModule.cs
@@ -1,13 +1,11 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Text;
-using Volo.Abp.Identity;
+﻿using Volo.Abp.Identity;
 using Volo.Abp.Modularity;
+
 
 namespace AbpBlog.Domain.Shared
 {
     [DependsOn(
-    typeof(AbpIdentityDomainModule)
+    typeof(AbpIdentityDomainSharedModule)
     )]
     public class AbpBlogDomainSharedModule : AbpModule
     {
--- src/AbpBlog.Domain/AbpBlogDomainModule.cs
+++ src/AbpBlog.Domain/AbpBlogDomainModule.cs
@@ -1,10 +1,12 @@
-﻿using Volo.Abp.Identity;
+﻿using AbpBlog.Domain.Shared;
+using Volo.Abp.Identity;
 using Volo.Abp.Modularity;
 
 namespace AbpBlog
 {
     [DependsOn(
-        typeof(AbpIdentityDomainModule)
+        typeof(AbpIdentityDomainModule),
+        typeof(AbpBlogDomainSharedModule)
         )]
     public class AbpBlogDomainModule : AbpModule
     {
