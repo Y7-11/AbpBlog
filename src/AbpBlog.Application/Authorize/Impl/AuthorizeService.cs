da22de45bb0c861dd89c05820e2628ab5699cd3e
叶俊泽 <838079577@qq.com>
2020-07-24 16:20:19 +0800

    修正{}缺少

--- src/AbpBlog.Application/Authorize/Impl/AuthorizeService.cs
+++ src/AbpBlog.Application/Authorize/Impl/AuthorizeService.cs
@@ -44,6 +44,7 @@ namespace AbpBlog.Application.Authorize.Impl
             }
             catch (Exception e)
             {
+
                 result.IsFailed(e.Message);
                 return result;
             }
@@ -92,54 +93,49 @@ namespace AbpBlog.Application.Authorize.Impl
         }
 
 
-
-
-
-    }
-
-    public async Task<ServiceResult<string>> GetAccessTokenAsync(string code)
-    {
-        var result = new ServiceResult<string>();
-        if (string.IsNullOrEmpty(code))
+        public async Task<ServiceResult<string>> GetAccessTokenAsync(string code)
         {
-            result.IsFailed("code为空");
+            var result = new ServiceResult<string>();
+            if (string.IsNullOrEmpty(code))
+            {
+                result.IsFailed("code为空");
+                return result;
+            }
+
+            var request = new AccessTokenRequest();
+
+            var content = new StringContent($"code={code}&client_id={request.Client_ID}&redirect_uri={request.Redirect_Uri}&client_secret={request.Client_Secret}");
+            content.Headers.ContentType = new MediaTypeHeaderValue("application/x-www-form-urlencoded");
+
+            var client = _httpClient.CreateClient();
+            var httpResponse = await client.PostAsync(GitHubConfig.API_AccessToken, content);
+
+            var response = await httpResponse.Content.ReadAsStringAsync();
+
+            if (response.StartsWith("access_token"))
+                result.IsSuccess(response.Split("=")[1].Split("&").First());
+            else
+                result.IsFailed("code不正确");
+
             return result;
         }
 
-        var request = new AccessTokenRequest();
-
-        var content = new StringContent($"code={code}&client_id={request.Client_ID}&redirect_uri={request.Redirect_Uri}&client_secret={request.Client_Secret}");
-        content.Headers.ContentType = new MediaTypeHeaderValue("application/x-www-form-urlencoded");
-
-        var client = _httpClient.CreateClient();
-        var httpResponse = await client.PostAsync(GitHubConfig.API_AccessToken, content);
-
-        var response = await httpResponse.Content.ReadAsStringAsync();
-
-        if (response.StartsWith("access_token"))
-            result.IsSuccess(response.Split("=")[1].Split("&").First());
-        else
-            result.IsFailed("code不正确");
-
-        return result;
-    }
-
-    public async Task<ServiceResult<string>> GetLoginAddressAsync()
-    {
-        var result = new ServiceResult<string>();
-        var request = new AuthorizeRequest();
-
-        var address = string.Concat(new string[]
+        public async Task<ServiceResult<string>> GetLoginAddressAsync()
         {
+            var result = new ServiceResult<string>();
+            var request = new AuthorizeRequest();
+
+            var address = string.Concat(new string[]
+            {
                 GitHubConfig.API_Authorize,
                 "?client_id=",request.Client_ID,
                 "&scope=", request.Scope,
                 "&state=", request.State,
                 "&redirect_uri=", request.Redirect_Uri
-        });
+            });
 
-        result.IsSuccess(address);
-        return await Task.FromResult(result);
+            result.IsSuccess(address);
+            return await Task.FromResult(result);
+        }
     }
-}
 }
